d0480fe05b1de9745232f777881a876113486549
KuvanovaOlga <olga.kuvanova99@gmail.com>
2018-08-14 14:35:53 +0300

    filesystem
    @kuvanovaolga

--- cwp-01/filesystem.js
+++ cwp-01/filesystem.js
@@ -97,3 +97,5 @@ function watchDir(dirForTxt) {
         }
     });
 }
+
+
--- cwp-01/index1.js
+++ cwp-01/index1.js
@@ -1,4 +1,8 @@
-for(var i=2; i<process.argv.length; i++){
-    var name = process.argv[i];
-    console.log(`Hi ${name}!`);
-}
+//for(var i=2; i<process.argv.length; i++){
+//    var name = process.argv[i];
+//    console.log(`Hi ${name}!`);
+//}
+
+process.argv.forEach((value)=> {
+    console.log(`${value}`);
+});
