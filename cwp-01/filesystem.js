6faa0c6a8ac842ae088aeb05dd93905c36acc392
KuvanovaOlga <olga.kuvanova99@gmail.com>
2018-09-26 22:50:41 +0300

    update
    @kuvanovaolga

--- cwp-01/filesystem.js
+++ cwp-01/filesystem.js
@@ -1,9 +1,9 @@
-const fs = require('fs'); //модуль, содержащий ф-ции для работы с файлами и директориями
-const path = require('path'); //предоставляет утилиты для работы с путями к файлам и директориям
-let pat = process.argv[2];
-let copyright;
+const fs = require('fs');
+const path = require('path');
 
-copyright = JSON.parse(fs.readFileSync("./cop.json")).cop;
+const pat = process.argv[2];
+
+const copyright = JSON.parse(fs.readFileSync("./cop.json")).cop;
 
 let script='const fs = require(\'fs\');\n' +
     'const path = require(\'path\');\n' +
@@ -24,11 +24,11 @@ let script='const fs = require(\'fs\');\n' +
     '    });\n' +
     '})(__dirname, null);\n';
 
-if(pat)
-{
+if(pat) {
     fs.stat(pat,function (error, statistics) {
-        if(error){console.log('path error');}
-        else{
+        if(error){
+            console.log('path error');
+        } else{
             create_summary(pat);
             make_dir(pat);
             watchDir(pat);
@@ -36,8 +36,7 @@ if(pat)
     })
 }
 
-function create_summary(pat)
-{
+function create_summary(pat) {
     fs.writeFile(pat+'\\Summary.js', script, function (error) {
             if (error) console.log('Error creating file.');
             else console.log('Summary created.');
@@ -45,37 +44,29 @@ function create_summary(pat)
     );
 }
 
-function make_dir(pat)
-{
+function make_dir(pat) {
     let dir_for_make=pat+'\\'+path.basename(pat);
-    fs.mkdir(dir_for_make, function(err)
-    {
+    fs.mkdir(dir_for_make, function(err){
         if(err) console.log('Error creating directory.');
         else console.log('Directory created.');
     });
     writefile(pat,dir_for_make);
 }
 
-function writefile(pat, made_dir)
-{
-    fs.readdir(pat, function(err, files)
-        {if(err) console.log('Error reading the directory.');
-        else
-        {
-            for(let i in files)
-            {
+function writefile(pat, made_dir){
+    fs.readdir(pat, function(err, files){
+        if(err) console.log('Error reading the directory.');
+        else {
+            for(let i in files){
                 let files_or_directories=pat+'\\'+files[i];
                 if(fs.statSync(files_or_directories).isDirectory())
                     writefile(files_or_directories,made_dir);
-                else
-                {
-                    if(path.extname(files_or_directories)===".txt")
-                    {
+                else {
+                    if(path.extname(files_or_directories)===".txt") {
                         fs.readFile(files_or_directories,'utf8' ,(err, data)=>{
                                 if(err)console.log('Error reading file.');
-                                else
-                                {
-                                    fs.writeFile(made_dir+'\\'+files[i], copyright+'\r\n'+data+'\r\n'+copyright );
+                                else {
+                                    fs.writeFile(made_dir + `\\` + files[i], copyright+`\r\n`+ data + `\r\n` + copyright );
                                 }
                             }
                         );
