bb827c67a45ddae2a25d8d502131280ea37587c6
Olga Kuvanova <31654272+KuvanovaOlga@users.noreply.github.com>
2019-02-18 08:53:51 +0300

    Add files via upload

--- cwp-22/index.js
+++ cwp-22/index.js
@@ -1,4 +1,4 @@
-const info = require ('.package.json');
+const info = require ('./package.json');
 console.log('Hello, I am ' + info.version);
 
 let operators = {
@@ -8,10 +8,46 @@ let operators = {
     '/': function(a, b) { return b / a },
 };
 
+let isOperator = function(token) {
+    return token in operators
+};
+
+let isValue = function(token) {
+    return /\d+/.test(token)
+};
+
 module.exports = function(notation){
     notation = notation.trim();
 
     if(notation === ""){
         return null;
     }
+    let tokens = notation.split(' ');
+
+    let stack = [];
+    while (tokens.length) {
+        let token = tokens.shift();
+
+        if (isValue(token)) {
+            stack.push(Number(token))
+
+        }
+        else if (isOperator(token)) {
+            let a = stack.pop();
+            let b = stack.pop();
+            if (a === null || b === null){
+                return null ;
+            }
+            stack.push(operators[token](a, b))
+        }
+        else {
+            return null;
+        }
+    }
+
+    if (stack.length !== 1) {
+        return null;
+    }
+
+    return stack.pop();
 };
